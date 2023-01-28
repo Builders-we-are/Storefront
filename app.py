21c58fb870591c6982d84ace63d67bf4664d4853
king-Ob <oxi8927@rit,edu>
2023-01-28 11:11:24 -0500

    basic web design for project

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask, render_template
+
+
 
 app = Flask(__name__)
 
@@ -8,9 +12,28 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+def get_db_connections():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/hi')
 def hi_nigeria():
     return 'Hi Nigeria'
+
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/menu')
+def menus():
+    conn = get_db_connections()
+    menus = conn.execute('SELECT * FROM menus').fetchall()
+    conn.close()
+    return render_template('menu.html', menus=menus)
+
 
 
 if __name__ == '__main__':
