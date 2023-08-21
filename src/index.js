3fc1e0d25b89ba8eb6f9ea09d4949d5df38c2660
Builders-we-are <112954060+Builders-we-are@users.noreply.github.com>
2023-08-21 14:34:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,3 +21,49 @@ Cesium.CzmlDataSource.load(czmlPath).then(dataSource => {
     viewer.dataSources.add(dataSource);
     viewer.zoomTo(dataSource);
 });
+
+
+
+// ... existing imports ...
+
+const czmlDropdown = document.getElementById('czmlDropdown');
+const czmlFileInput = document.getElementById('czmlFileInput');
+const uploadButton = document.getElementById('uploadButton');
+
+// ... existing code ...
+
+// Event listener for the "Upload" button
+uploadButton.addEventListener('click', () => {
+    const selectedFile = czmlFileInput.files[0];
+    if (selectedFile) {
+        uploadCzmlFile(selectedFile);
+    } else {
+        console.error('No file selected for upload.');
+    }
+});
+
+async function uploadCzmlFile(file) {
+    try {
+        // Create a FormData object to send the file
+        const formData = new FormData();
+        formData.append('czmlFile', file);
+
+        // Send a POST request to the server to handle the file upload
+        const response = await fetch('/upload-czml', {
+            method: 'POST',
+            body: formData,
+        });
+
+        if (response.ok) {
+            // File successfully uploaded, refresh the CZML file list
+            populateCzmlDropdown();
+        } else {
+            console.error('Error uploading CZML file.');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// ... existing code ...
+
